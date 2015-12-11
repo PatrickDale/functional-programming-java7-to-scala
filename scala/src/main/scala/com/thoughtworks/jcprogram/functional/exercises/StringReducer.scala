27ac0295550af70ad49e7da5418044955c68ecac
Bill Schofield <bill@billschofield.net>
2015-12-11 16:33:25 -0600

    Grammar fixes

--- scala/src/main/scala/com/thoughtworks/jcprogram/functional/exercises/StringReducer.scala
+++ scala/src/main/scala/com/thoughtworks/jcprogram/functional/exercises/StringReducer.scala
@@ -1,7 +1,7 @@
 package com.thoughtworks.jcprogram.functional.exercises
 
 // Given a list of strings like {"Bill", "Archer", "Lana"}
-// Print the first letter of each of the string that have a length of 4.
+// Print the first letter of each of the strings that has a length of 4.
 // For example, "BL"
 
 object StringReducer {
