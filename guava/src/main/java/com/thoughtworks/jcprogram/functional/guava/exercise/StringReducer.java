7e22f6695ac9a5760b313c381e7af95194d5f2b3
Pat Dale <pdale@thoughtworks.com>
2015-12-15 16:37:38 -0600

    finished guava exercises

--- guava/src/main/java/com/thoughtworks/jcprogram/functional/guava/exercise/StringReducer.java
+++ guava/src/main/java/com/thoughtworks/jcprogram/functional/guava/exercise/StringReducer.java
@@ -1,8 +1,14 @@
 package com.thoughtworks.jcprogram.functional.guava.exercise;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+
 import java.util.List;
 
+import static com.google.common.collect.Collections2.filter;
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.transform;
 
 // Given a list of strings like {"Bill", "Archer", "Lana"}
 // Print the first letter of each of the strings that has a length of 4.
@@ -17,7 +23,21 @@ public class StringReducer {
         System.out.println(new StringReducer().reduceToFirstLetterOfLengthFourStrings(strings));
     }
 
+    private static class FirstCharacterInStringFunction implements Function<String, String> {
+        public String apply(String string) {
+            return String.valueOf(string.charAt(0));
+        }
+    }
+
+    private static class IsLengthFourPredicate implements Predicate<String> {
+        public boolean apply(String string) {
+            return string.length() == 4;
+        }
+    }
+
     public String reduceToFirstLetterOfLengthFourStrings(List<String> strings) {
-        return "";
+        List<String> lengthFourStrings = newArrayList(filter(strings, new IsLengthFourPredicate()));
+        List<String> firstCharacters = transform(lengthFourStrings, new FirstCharacterInStringFunction());
+        return Joiner.on("").join(firstCharacters);
     }
 }
